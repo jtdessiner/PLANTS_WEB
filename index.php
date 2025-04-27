0a1c10438db4fac11268ad234415b79da1145f9f
jtdessiner <joseto.valverde@gmail.com>
2025-04-27 23:46:49 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php include ('layouts/header.php'); ?>
+<!--Hero-->
+   <div class="container-fluid mt-5 py-5 px-5";>
+    <div class="row align-items-center">
+      <div class="col-9">
+        <h2 class="display-1 text-start fw-medium lh-1">A collection of <br> my favorites plants</h2>
+      </div>
+      <div class="col-3">
+        <p>columns</p>
+      </div>
+    </div>
+  
+   </div>
+<!--Hero-->
+   
+     <!--Carousel-->
+  <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="img/planta_home.jpg" class="d-block w-100 img-fluid" alt="...">
+      </div>
+      <div class="carousel-item">
+        <img src="img/planta_home.jpg" class="d-block w-100" alt="...">
+      </div>
+      <div class="carousel-item">
+        <img src="img/planta_home.jpg" class="d-block w-100" alt="...">
+      </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
+   <!--Carousel-->
+      
+    
+<script src="dist/js/bootstrap.bundle.min.js"></script>
+
+</body>
+</html>
